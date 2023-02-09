6a1fe5b99182913e9646d8c9c6866006e4355a45
Mamaniyozov <mamaniyozovmuhammadyusuf5@gmail.com>
2023-02-09 03:10:02 -0800

    teble

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -12,10 +13,34 @@ def hello_world():
 
 @app.route('/home')
 def home():
-    return 'Home Page!!'
+    return 'Home Page!!;)'
 @app.route('/about')
 def about():
-    return ("welcome to my web sayt").title()
+    return ("welcome to my web sayt:)").title()
+@app.route('/table')
+def table():
+    # Import pandas package
+
+
+# Define a dictionary containing Students data
+    data = {'Name': ['Jai', 'Princi', 'Gaurav', 'Anuj'],
+            'Height': [5.1, 6.2, 5.1, 5.2],
+            'Qualification': ['Msc', 'MA', 'Msc', 'Msc']}
+
+    # Convert the dictionary into DataFrame
+    df = pd.DataFrame(data)
+
+    # Declare a list that is to be converted into a column
+    address = ['Delhi', 'Bangalore', 'Chennai', 'Patna']
+
+    # Using 'Address' as the column name
+    # and equating it to the list
+    df['Address'] = address
+
+# Observe the result
+    return df
+
+
 
 if __name__ == '__main__':
     # Run the app in local network
